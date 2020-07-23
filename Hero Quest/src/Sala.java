ebd441738d88f8963ccd0a3e56882070824a3d65
Matheus <matheusleal9696@gmail.com>
2020-07-23 20:42:03 -0300

    sadasd

--- Hero Quest/src/Sala.java
+++ Hero Quest/src/Sala.java
@@ -1,9 +1,23 @@
 import java.util.ArrayList;
 
 public class Sala {
-	private ArrayList<Posicao> celulas = new ArrayList<Posicao>();
+	private ArrayList<Tesouro> tesouros = new ArrayList<Tesouro>();
+	private ArrayList<Armadilha> armadilhas = new ArrayList<Armadilha>();
+	private ArrayList<Monstro> monstros = new ArrayList<Monstro>();
 	
-	public Sala(ArrayList<Posicao> celulas) {
-		this.celulas = celulas;
+	public Sala(ArrayList<Tesouro> tesouros, ArrayList<Armadilha> armadilhas, ArrayList<Monstro> monstros) {
+		this.tesouros = tesouros;
+		this.armadilhas = armadilhas;
+		this.monstros = monstros;
+	}
+	
+	public ArrayList<Tesouro> getTesouros(){
+		return this.tesouros;
+	}
+	public ArrayList<Armadilha> getArmadilhas(){
+		return this.armadilhas;
+	}
+	public ArrayList<Monstro> getMonstros(){
+		return this.monstros;
 	}
 }
