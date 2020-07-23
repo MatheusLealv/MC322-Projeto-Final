06280942a4d843a3d3cb60b25ae681bbce779d02
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-07-23 18:59:32 -0300

    Barbarian

--- Hero Quest/src/Barbaro.java
+++ Hero Quest/src/Barbaro.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Barbaro extends Heroi{
 
 	public Barbaro(String nome) {
-		super(nome, 3, 2, 8, 2, new ArrayList<Arma>(new EspadaLonga()));
+		super(nome, 3, 2, 8, 2, new ArrayList<Arma>());
+		addArma(new EspadaLonga(0));
 	}
-
+	
 }
--- Hero Quest/src/EspadaLonga.java
+++ Hero Quest/src/EspadaLonga.java
@@ -1,7 +1,6 @@
 
 public class EspadaLonga extends Arma{
 
-
 	public EspadaLonga(int bonusDado) {
 		super(bonusDado, 2,  false);
 	}
