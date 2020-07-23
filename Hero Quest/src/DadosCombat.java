20e01c9662dcfc8479be67b021b9837fba72c8f9
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-07-23 18:01:13 -0300

    Commit dado de combate.

--- Hero Quest/src/DadosCombat.java
+++ Hero Quest/src/DadosCombat.java
@@ -3,6 +3,15 @@ import java.util.Random;
 public class DadosCombat implements Dados {
 	public int roll() {
 		Random rng = new Random();
-		return rng.nextInt(6) + 1;
+		int u = rng.nextInt(6) + 1;
+		if(u <= 3) {
+			return 0; // Caveira
+		}
+		else if(u < 6) {
+			return 1; // Escudo do heroi
+		}
+		else {
+			return 2; // Escudo do monstro
+		}
 	}
 }
