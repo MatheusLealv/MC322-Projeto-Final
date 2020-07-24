d347ace215b64c1202683cb19b5eb86103493c01
yanvictor <yn.guimaraes@gmail.com>
2020-07-23 21:21:32 -0300

    adicionando Unit

---
+++ Hero Quest/src/Unit.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class Unit {
+	private int numDadosAtaque;
+	private int numDadosDefesa;
+	private int pontosVida;
+	private int pontosInteligencia;
+	
+	//private Armadura armadura;
+	private ArrayList<Arma> armas = new ArrayList<Arma>();
+	
+	public Unit(int numDadosAtaque, int numDadosDefesa, int pontosVida, int pontosInteligencia, ArrayList<Arma> armas) {
+		//this.nome = nome;
+		this.numDadosAtaque = numDadosAtaque;
+		this.numDadosDefesa = numDadosDefesa;
+		this.pontosVida = pontosVida;
+		this.pontosInteligencia = pontosInteligencia;
+		this.armas = armas;
+		
+	}
+	
+	public void addArma(Arma arma) {
+		//LEMBRAR DE MELHORAR ISSO
+		if(armas.size() == 2) {
+			System.out.println("Você já possui duas armas");
+			return;
+		}
+		this.armas.add(arma);
+	}
+	
+	public void removeArma(Arma arma) {
+		//LEMBRAR DE MELHORAR
+		if(armas.contains(arma)) {
+			this.armas.remove(arma);
+		}
+		else {
+			System.out.println("Você não possui essa arma");
+		}
+	}
+	
+	public void atualizarArmadura(Armadura armadura) {
+		armadura = armadura;
+	}
+	//ATUALIZAR PONTOS DE INTELIGENCIA
+	public void atualizarPI(int pontosInteligencia) {
+		this.pontosInteligencia = pontosInteligencia;
+	}
+	//ATUALIZAR PONTOS DE VIDA
+	public void atualizarPV(int pontosVida) {
+		this.pontosVida = pontosVida;
+	}
+	public void atualizarAtaque(int numDadosAtaque) {
+		this.numDadosAtaque = numDadosAtaque;
+	}
+	public void atualizarDefesa(int numDadosDefesa) {
+		this.numDadosDefesa = numDadosDefesa;
+	}	
+	
+	public int getNumDadosAtaque() {
+		return this.numDadosAtaque;
+	}
+	public int getNumDadosDefesa() {
+		return this.numDadosDefesa;
+	}
+	public int getPontosVida() {
+		return this.pontosVida;
+	}
+	public int getPontosInteligencia() {
+		return this.pontosInteligencia;
+	}
+	
+	public ArrayList<Arma> getArmas(){
+		return this.armas;
+	}
+}
