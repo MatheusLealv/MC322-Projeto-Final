3d583c242267a85c1a144c5aa3846f40e2c3cb63
Matheus <matheusleal9696@gmail.com>
2020-07-23 22:14:04 -0300

    simpleheal magia

--- Hero Quest/src/SimpleHeal.java
+++ Hero Quest/src/SimpleHeal.java
@@ -1,7 +1,11 @@
 
 public class SimpleHeal extends Magia{
-
+	Unit magico;
 	public SimpleHeal(Unit magico) {
+		this.magico = magico;
+	}
+	
+	public void usarMagia() {
 		if(usarMagia(magico)) {
 			DadosRed dado = new DadosRed();
 			int valorDado = dado.roll();
