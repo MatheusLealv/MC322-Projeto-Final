526b2a0b8178924354a0616b1d1b05a5b3539bee
Matheus Leal <matheusleal9696@gmail.com>
2020-07-25 19:13:39 -0300

    asdasda

--- Hero Quest/src/Game.java
+++ Hero Quest/src/Game.java
@@ -10,14 +10,10 @@ public class Game {
 		Heroi heroi = Interaction.criarPersonagem();
 		mapa.generateMap(heroi);		
 		
-		for(int i = 0; i < 10; i++) {
-			//Scanner read = new Scanner(System.in);
-			//int x = read.nextInt();
+		while(true) {
 			mapa.printMap();
-			//Interaction.turnoHeroi(heroi, mapa);
+			Interaction.turnoHeroi(heroi, mapa);
 			Interaction.turnoMonstros(mapa);
-			System.out.println();
-			System.out.println("SUA VIDA = " + heroi.getPontosVida());
 		}
 	}
 }
--- Hero Quest/src/Interaction.java
+++ Hero Quest/src/Interaction.java
@@ -34,7 +34,7 @@ public class Interaction {
 		int roll = dado.roll();
 		roll += dado.roll();
 		mapa.moveHeroi(heroi, roll);
-		while(true) {
+		//while(true) {
 			Scanner read = new Scanner(System.in);
 			while(true) {
 				System.out.println("Escolha Sua Ação:");
@@ -60,7 +60,7 @@ public class Interaction {
 					System.out.println("Por favor digite uma ação válida");
 				}
 			}
-		}
+	//	}
 		
 		//heroi.move(mapa, roll);
 	}
