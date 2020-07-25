a74dc9218da7eaa5d34beafc210916fcf0188a84
yanvictor <yn.guimaraes@gmail.com>
2020-07-25 14:44:59 -0300

    upd

---
+++ Hero Quest/src/Game.java
@@ -0,0 +1,20 @@
+
+public class Game {
+
+	public static void start() {
+		// ------ TESTES -------
+		int N = 5, M = 5; // MODIFICAR
+		Mapa mapa = new Mapa(N, M);
+				
+		Heroi heroi = Interaction.criarPersonagem();
+		
+		//Esqueleto esqueleto = new Esqueleto(0,1);
+		//System.out.println(heroi.getNome() + " " + esqueleto.getPontosVida());
+				
+		mapa.generateMap(heroi);
+		System.out.println("DONE\n");
+				
+		mapa.printMap();
+	}
+
+}
---
+++ Hero Quest/src/Interaction.java
@@ -0,0 +1,10 @@
+
+public class Interaction {
+
+	public static Heroi criarPersonagem() {
+		System.out.println("Bem vindo a Hero Quest");
+		System.out.println("Digite o número correspondente a classe desejada");
+		System.out.println("0-Bárbaro 1-Anão 2-Elfo 3-Feiticeiro");
+	}
+
+}
