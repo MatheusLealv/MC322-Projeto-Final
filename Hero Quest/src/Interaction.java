cb2743cac31f1dbc78f5bfbaee7b767fd7ebc5bd
Matheus Leal <matheusleal9696@gmail.com>
2020-07-25 18:04:37 -0300

    interact

--- Hero Quest/src/Interaction.java
+++ Hero Quest/src/Interaction.java
@@ -37,13 +37,83 @@ public class Interaction {
 		//heroi.move(mapa, roll);
 	}
 	
-	public void realizarAtaque(Mapa mapa, Heroi heroi) {
-		int[] dx = {1, -1, 0, 0};
-		int[] dy = {0, 0, 1, -1};
+	public void utilizarPunhal(Mapa mapa, Heroi heroi, int qtdPunhal) {
+		Scanner read = new Scanner(System.in);
+		int[] dx = {1, -1, 0, 0, 2, -2, 0, 0, 3, -3, 0, 0};
+		int[] dy = {0, 0, 1, -1, 0, 0, 2, -2, 0, 0, 3, -3};	
 		
+		ArrayList<Arma> minhasArmas = new ArrayList<Arma>();
+		
+		for(Arma arma: heroi.getArmas()) {
+			if((arma instanceof Punhal) && (qtdPunhal > 0)) {
+				qtdPunhal --;
+				minhasArmas.add(arma);
+			}
+		}
+		
+		for(int punhal = 0; punhal < qtdPunhal; punhal ++) {
+			System.out.println("Digite o número correspondente ao monstro que deseja apunhalar");
+			ArrayList<Monstro> monstros = new ArrayList<>();			
+			for(int i = 0; i < 12; i++) {
+				int x = heroi.getX() + dx[i], y = heroi.getY() + dy[i];
+				if(0 <= x && x < mapa.getN() && 0 <= y && y < mapa.getM()) {
+					Celula C = mapa.getCelula(x, y);
+					if(C instanceof Monstro) {
+						monstros.add((Monstro) C);
+					}
+				}
+			}
+			int i = 0;
+			for(Monstro monstro: monstros) {
+				System.out.println(i + " - " + monstro + " at position " + monstro.getX() + ", " + monstro.getY());
+				i ++;
+			}
+			
+			int operacao = read.nextInt();
+			while(operacao < 0 || operacao >= monstros.size()) {
+				System.out.println("Opção inválida! Digite novamente");
+				operacao = read.nextInt();				
+			}
+			
+			Monstro monstro = monstros.get(operacao);
+			heroi.combate(monstro, minhasArmas);
+			
+		}
+		
+	}
+	public void realizarAtaque(Mapa mapa, Heroi heroi) {
+		Scanner read = new Scanner(System.in);
+		int[] dx = {1, -1, 0, 0, 2, -2, 0, 0};
+		int[] dy = {0, 0, 1, -1, 0, 0, 2, -2};
+		
+		int qtdPunhal = 0;
+		ArrayList<Arma> minhasArmas = new ArrayList<Arma>();
+		for(Arma arma: heroi.getArmas()) {
+			if(arma instanceof Punhal) {
+				qtdPunhal ++;				
+			}
+			else {
+				minhasArmas.add(arma);
+			}
+		}
+		
+		System.out.println("Você possui " + qtdPunhal + "punhais. Digite quantos você quer utilizar");
+		int punhaisUsaveis = read.nextInt();
+		while(punhaisUsaveis < 0 || punhaisUsaveis > qtdPunhal) {
+			punhaisUsaveis = read.nextInt();
+		}
+		
+		for(Arma arma: heroi.getArmas()) {
+			if( (arma instanceof Punhal) && punhaisUsaveis > 0) {
+				punhaisUsaveis --;
+				minhasArmas.add(arma);
+			}
+		}
+			
 		ArrayList<Monstro> monstros = new ArrayList<Monstro>();
 		for(int i = 0; i < 4; i++) {
 			int x = heroi.getX() + dx[i], y = heroi.getY() + dy[i];
+			int distancia = Math.abs(x - heroi.getX()) + Math.abs(y - heroi.getY());
 			if(0 <= x && x < mapa.getN() && 0 <= y && y < mapa.getM()) {
 				Celula C = mapa.getCelula(x, y);
 				if(C instanceof Monstro) {
@@ -53,7 +123,6 @@ public class Interaction {
 		}
 		int i = 0;
 		if(monstros.size() >= 1) {
-			Scanner read = new Scanner(System.in);
 			System.out.println("Digite o número correspondente ao monstro que deseja atacar");
 			for(Monstro monstro: monstros) {
 				System.out.println(i + " - " + monstro + " at position " + monstro.getX() + ", " + monstro.getY());
@@ -66,7 +135,7 @@ public class Interaction {
 			}
 			
 			Monstro monstro = monstros.get(operacao);
-			heroi.combate(monstro);
+			heroi.combate(monstro, minhasArmas);
 		}
 		else {
 			System.out.println("Não há monstros para serem atacados");
