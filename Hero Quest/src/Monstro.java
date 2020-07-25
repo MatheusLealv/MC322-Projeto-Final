88bc3c5c6ec0644a0543f2b9b70174a4b30bb195
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-07-25 19:40:02 -0300

    .

--- Hero Quest/src/Monstro.java
+++ Hero Quest/src/Monstro.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Monstro extends Unit{
+public class Monstro extends Unit implements Dropavel{
 	public Monstro(int numDadosAtaque, int numDadosDefesa, int pontosVida, int pontosInteligencia, ArrayList<Arma> armas, ArrayList<Magia> magias, int posx , int posy) {
 		super(numDadosAtaque,numDadosDefesa,pontosVida,pontosInteligencia,armas, magias, posx,posy);
 		
