3226118333e128bcddaeaf2d6845595bbd1b73e6
Matheus Leal <matheusleal9696@gmail.com>
2020-07-26 18:46:28 -0300

    kaka

--- Hero Quest/src/Celula.java
+++ Hero Quest/src/Celula.java
@@ -22,39 +22,39 @@ public class Celula {
 	}
 	
 	public boolean isVisible(Mapa mapa, Celula C) {
+		//return true;
+		if(!atrapalhaVisao(C)) {
+			return true;
+		}
+		if(C.getX() != this.x && C.getY() != this.y) {
+			return false;
+		}
+		
+		if(C.getX() == this.x) {
+			int minY = Math.min(this.y, C.getY());
+			int maxY = Math.max(this.y, C.getY());
+			
+			for(int j = minY + 1; j < maxY; j++) {
+				Celula cel = mapa.getCelula(this.x, j);
+				if( (cel instanceof Muro) || (cel instanceof Porta) || (cel instanceof Monstro)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		int minX = Math.min(this.x, C.getX());
+		int maxX = Math.max(this.x, C.getX());
+		
+		if(C.getX() == 2 && C.getY() == 11) {
+			System.out.println("CAGADA");
+		}
+		for(int j = minX + 1; j < maxX; j++) {
+			Celula cel = mapa.getCelula(j, this.y);
+			if( (cel instanceof Muro) || (cel instanceof Porta) || (cel instanceof Monstro)) {
+				return false;
+			}
+		}
 		return true;
-//		if(!atrapalhaVisao(C)) {
-//			return true;
-//		}
-//		if(C.getX() != this.x && C.getY() != this.y) {
-//			return false;
-//		}
-//		
-//		if(C.getX() == this.x) {
-//			int minY = Math.min(this.y, C.getY());
-//			int maxY = Math.max(this.y, C.getY());
-//			
-//			for(int j = minY + 1; j < maxY; j++) {
-//				Celula cel = mapa.getCelula(this.x, j);
-//				if( (cel instanceof Muro) || (cel instanceof Porta) || (cel instanceof Monstro)) {
-//					return false;
-//				}
-//			}
-//			return true;
-//		}
-//		int minX = Math.min(this.x, C.getX());
-//		int maxX = Math.max(this.x, C.getX());
-//		
-//		if(C.getX() == 2 && C.getY() == 11) {
-//			System.out.println("CAGADA");
-//		}
-//		for(int j = minX + 1; j < maxX; j++) {
-//			Celula cel = mapa.getCelula(j, this.y);
-//			if( (cel instanceof Muro) || (cel instanceof Porta) || (cel instanceof Monstro)) {
-//				return false;
-//			}
-//		}
-//		return true;
 	}
 	
 	protected void atualizarPos(int x , int y) {
